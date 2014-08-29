5a22ba60640dd6e50e35b392b79079d35c6aa151
Anand Kuchibotla <akuchibotla@gmail.com>
2014-08-29 02:02:19 -0700

    Better error catching

--- rename_script.py
+++ rename_script.py
@@ -1,8 +1,18 @@
 import sys
 import os
 
-if len(sys.argv) != 2:
-	raise RuntimeError("Insufficient Arguments: must provide one and only one path")
+if len(sys.argv) < 2:
+	raise SystemExit("Insufficient Arguments: must provide an argument\n\nCorrect Format:\npython <path to script>/rename_script.py <path to files to be renamed> <optional prefix> <optional suffix>")
+
+elif len(sys.argv) == 3:
+	prefix = sys.argv[2]
+
+elif len(sys.argv) == 4:
+	prefix = sys.argv[2]
+	suffix = sys.argv[3]
+
+else:
+	raise SystemExit("Insufficient Arguments: must provide at most 3 arguments in order\n\nCorrect Format:\npython <path to script>/rename_script.py <path to files to be renamed> <optional prefix> <optional suffix>")
 
 path = str(sys.argv[1])
 print path
@@ -10,7 +20,7 @@ print path
 files = os.listdir(path)
 
 if files == 256:
-	raise RuntimeError("Incorrect path")
+	raise SystemExit("Incorrect path")
 
 counter = 0
 
@@ -24,7 +34,7 @@ for f in files:
 	print bash_command
 	val = os.system(bash_command)
 	if val != 0:
-		raise RuntimeError("Error in bash_command ", bash_command)
+		raise SystemExit("Error in bash_command ", bash_command)
 	print "Done"
 	print ""
 	counter += 1
